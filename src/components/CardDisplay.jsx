8c559bcfeabb636e56196892e727cb1f9d3f5783
AbhayBhan <abhaybhan197@gmail.com>
2022-11-25 18:30:24 +0530

    CardDisplay Component completed

--- src/components/CardDisplay.jsx
+++ src/components/CardDisplay.jsx
@@ -11,9 +11,24 @@ export const CardDisplay = () => {
         <div className='FrontCard-Box'>
           <img className='Card-Front' src={cardFront}></img>
           <img className='Card-Logo' src={cardLogo}></img>
+          <div className='Card-Details'>
+            <div className='Number-Display'>
+              <h1>0000</h1>
+              <h1>0000</h1>
+              <h1>0000</h1>
+              <h1>0000</h1>
+            </div>
+            <div className='Person-Info'>
+              <h2>Abhay Bhan</h2>
+              <h2>00/00</h2>
+            </div>
+          </div>
         </div>
         <div className='RearCard-Box'>
           <img className='Card-Rear' src={cardRear}></img>
+          <div className='CVV-Display'>
+            <h4>000</h4>
+          </div>
         </div>
     </div>
   )
